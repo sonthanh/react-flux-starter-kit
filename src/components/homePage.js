d2692c87e179c085f746844369fb63b2fd43bb94
sonthanh <sonthanhdo2004@gmail.com>
2015-09-21 21:59:39 +0200

    Update

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,14 +3,16 @@
  */
 "use strict";
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
   render: function () {
     return (
       <div className="jumbotron">
         <h1>H1</h1>
-
         <p>abc</p>
+        <Link to="about" className='btn btn-primary btn-large'>Learn more </Link>
       </div>
     );
   }
